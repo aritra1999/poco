e4b65947f48f8e3169491ee1dd458315caa5ef24
Aritra Mondal <mondalaritra002@gmail.com>
2023-02-10 14:31:47 +0530

    added navbar

--- src/components/navbar/Navbar.svelte
+++ src/components/navbar/Navbar.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-    import { DarkMode } from 'flowbite-svelte';
-    import Search from '../search/Search.svelte';
+    
+    import Search from './search/Search.svelte';
+    import Auth from './auth/Auth.svelte';
+	import { DarkMode } from 'flowbite-svelte';
 </script>
 
-<nav class="w-screen h-16 flex items-start justify-between ">
-    <div class="h-full flex space-x-2 items-center text-xl font-semibold">
+<nav class="h-16 p-4 flex items-start justify-between text-zinc-50 my-4 ackdrop-blur-md bg-zinc-800/30">
+    <div class="h-full flex space-x-2 items-center text-xl font-semibold px-3">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z" />
         </svg>          
         <h2>p0co</h2>
     </div>
     <div class="h-full flex space-x-4 items-center">
-        <Search />
         <DarkMode />
+        <Search />
+        <Auth />
     </div>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,5 +3,10 @@
 	import Navbar from '../components/navbar/Navbar.svelte';
 </script>
 
-<Navbar/>
-<slot />
+<div class="bg"></div>
+<div class="container mx-auto">
+	<Navbar/>
+	<div class="bg-zinc-100 text-zinc-800 dark:text-zinc-100 dark:bg-zinc-900 rounded-md min-h-[90vh] shadow-2xl m-2">
+		<slot />
+	</div>
+</div>
