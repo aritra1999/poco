bbdbdfff5523c222ee7f4229f17aac5a41f06f29
Aritra Mondal <mondalaritra002@gmail.com>
2023-02-10 15:06:16 +0530

    layout and ui changes for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,22 @@
 <script>
+	import { DarkMode } from 'flowbite-svelte';
 	import '../app.postcss';
 	import Navbar from '../components/navbar/Navbar.svelte';
+	import { meta } from '../store/meta';
+
+    $: innerWidth = 0
+    $: meta.set({ screen: innerWidth < 850 ? 'mobile' : 'desktop' })
+
 </script>
+<svelte:window bind:innerWidth />
 
 <div class="bg"></div>
-<div class="container mx-auto">
+<div class="mx-auto">
 	<Navbar/>
-	<div class="bg-zinc-100 text-zinc-800 dark:text-zinc-100 dark:bg-zinc-900 rounded-md min-h-[90vh] shadow-2xl m-2">
-		<slot />
+	<div class="container mx-auto mt-20">
+		<div class="m-2 bg-zinc-100 text-zinc-800 dark:text-zinc-100 dark:bg-zinc-900 rounded-md min-h-[90vh] shadow-2xl">
+			<DarkMode />
+			<slot />
+		</div>
 	</div>
 </div>
