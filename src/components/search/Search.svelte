902d77b333a9c754ea65398b01c1dd99a553da95
Aritra Mondal <mondalaritra002@gmail.com>
2023-02-10 15:23:49 +0530

    cleanup

--- src/components/search/Search.svelte
+++ src/components/search/Search.svelte
@@ -1,5 +1,5 @@
 <div class="relative w-full">
-    <input type="text" class="text-sm block w-full pr-10 py-2.5 pl-5 bg-zinc-700 border-0 focus:ring-0 rounded-md" placeholder="search" required />
+    <input type="text" class="block pr-10 py-2.5 pl-5 rounded-md text-sm bg-zinc-700 border-0 focus:ring-0 sm:focus:w-96" placeholder="search" required />
     <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-zinc-400">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
